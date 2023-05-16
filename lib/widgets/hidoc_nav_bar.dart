722c08a6deec2ca6ee12c23162013cbfaf60b304
rounak-stellar <rounakraj@infostellar.com>
2023-05-16 10:44:32 +0530

    nav bar thing

--- lib/widgets/hidoc_nav_bar.dart
+++ lib/widgets/hidoc_nav_bar.dart
@@ -11,6 +11,7 @@ class HidocNavBar extends StatefulWidget {
 class _HidocNavBarState extends State<HidocNavBar> {
 
   List hoverValue = [false,false,false,false,false,false,false,false,false,false,false];
+  int selectedValue = 0;
 
 
   void resetAllValues(){
@@ -37,71 +38,170 @@ class _HidocNavBarState extends State<HidocNavBar> {
             children: [
               SizedBox(width: 100,),
               InkWell(
+                onHover: (value) {
+                  setState(() {
+                    if(selectedValue != 0){
+                      hoverValue[0] =  value? true: false;
+                    }
+                  });
+                },
                   onTap: () {
                     resetAllValues();
-                    hoverValue[0] = true;
+                    setState((){
+                      selectedValue = 0;
+                    });
                   },
-                  child: Text("Social",style: TextStyle(color: hoverValue[0] ? Colors.blue: Colors.black))),
+                  child: Text("Social",style: TextStyle(color: (hoverValue[0] || selectedValue == 0 ) ? Colors.blue: Colors.black))),
               Container(height: 30, margin: EdgeInsets.symmetric(horizontal: 5),color: Colors.black54,width: 3, ),
               InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      if(selectedValue != 1){
+                        hoverValue[1] =  value? true: false;
+                      }
+                    });
+                  },
                   onTap: () {
                     resetAllValues();
-                    hoverValue[1] = true;
-                  },child: Text("Cases",style: TextStyle(color: hoverValue[1] ? Colors.blue: Colors.black))),
+                    setState((){
+                      selectedValue = 1;
+                    });
+                  },child: Text("Cases",style: TextStyle(color: (hoverValue[1] || selectedValue == 1 ) ? Colors.blue: Colors.black))),
               Container(height: 30, margin: EdgeInsets.symmetric(horizontal: 5),color: Colors.black54,width: 3,),
               InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      if(selectedValue != 2){
+                        hoverValue[2] =  value? true: false;
+                      }
+                    });
+                  },
                   onTap: () {
                     resetAllValues();
-                    hoverValue[2] = true;
-                  },child: Text("Quizzes",style: TextStyle(color: hoverValue[2] ? Colors.blue: Colors.black))),
+                    setState((){
+                      selectedValue = 2;
+                    });
+                  },child: Text("Quizzes",style: TextStyle(color: (hoverValue[2] || selectedValue == 2 ) ? Colors.blue: Colors.black))),
               Container(height: 30, margin: EdgeInsets.symmetric(horizontal: 5),color: Colors.black54,width: 3,),
               InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      if(selectedValue != 3){
+                        hoverValue[3] =  value? true: false;
+                      }
+                    });
+                  },
                   onTap: () {
                     resetAllValues();
-                    hoverValue[3] = true;
-                  },child: Text("Articles",style: TextStyle(color: hoverValue[3] ? Colors.blue: Colors.black))),
+                    setState((){
+                      selectedValue = 3;
+                    });
+                  },child: Text("Articles",style: TextStyle(color: (hoverValue[3] || selectedValue == 3 ) ? Colors.blue: Colors.black))),
               Container(height: 30, margin: EdgeInsets.symmetric(horizontal: 5),color: Colors.black54,width: 3,),
               InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      if(selectedValue != 4){
+                        hoverValue[4] =  value? true: false;
+                      }
+                    });
+                  },
                   onTap: () {
                     resetAllValues();
-                    hoverValue[4] = true;
-                  },child: Text("Drugs",style: TextStyle(color: hoverValue[4] ? Colors.blue: Colors.black))),
+                    setState((){
+                      selectedValue = 4;
+                    });
+                  },child: Text("Drugs",style: TextStyle(color: (hoverValue[4] || selectedValue == 4 ) ? Colors.blue: Colors.black))),
               Container(height: 30, margin: EdgeInsets.symmetric(horizontal: 5),color: Colors.black54,width: 3,),
               InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      if(selectedValue != 5){
+                        hoverValue[5] =  value? true: false;
+                      }
+                    });
+                  },
                   onTap: () {
                     resetAllValues();
-                    hoverValue[5] = true;
-                  },child: Text("Webinars",style: TextStyle(color: hoverValue[5] ? Colors.blue: Colors.black))),
+                    setState((){
+                      selectedValue = 5;
+                    });
+                  },child: Text("Webinars",style: TextStyle(color: (hoverValue[5] || selectedValue == 5 ) ? Colors.blue: Colors.black))),
               Container(height: 30, margin: EdgeInsets.symmetric(horizontal: 5),color: Colors.black54,width: 3,),
               InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      if(selectedValue != 6){
+                        hoverValue[6] =  value? true: false;
+                      }
+                    });
+                  },
                   onTap: () {
                     resetAllValues();
-                    hoverValue[6] = true;
-                  },child: Text("Calculators",style: TextStyle(color: hoverValue[6] ? Colors.blue: Colors.black))),
+                    setState((){
+                      selectedValue = 6;
+                    });
+                  },child: Text("Calculators",style: TextStyle(color: (hoverValue[6] || selectedValue == 6 ) ? Colors.blue: Colors.black))),
               Container(height: 30, margin: EdgeInsets.symmetric(horizontal: 5),color: Colors.black54,width: 3,),
               InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      if(selectedValue != 7){
+                        hoverValue[7] =  value? true: false;
+                      }
+                    });
+                  },
                   onTap: () {
                     resetAllValues();
-                    hoverValue[7] = true;
-                  },child: Text("Guidelines",style: TextStyle(color: hoverValue[7] ? Colors.blue: Colors.black))),
+                    setState((){
+                      selectedValue = 7;
+                    });
+                  },child: Text("Guidelines",style: TextStyle(color: (hoverValue[7] || selectedValue == 7 ) ? Colors.blue: Colors.black))),
               Container(height: 30, margin: EdgeInsets.symmetric(horizontal: 5),color: Colors.black54,width: 3,),
               InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      if(selectedValue != 8){
+                        hoverValue[8] =  value? true: false;
+                      }
+                    });
+                  },
                   onTap: () {
                     resetAllValues();
-                    hoverValue[8] = true;
-                  },child: Text("Surveys",style: TextStyle(color: hoverValue[8] ? Colors.blue: Colors.black))),
+                    setState((){
+                      selectedValue = 8;
+                    });
+                  },child: Text("Surveys",style: TextStyle(color: (hoverValue[8] || selectedValue == 8 ) ? Colors.blue: Colors.black))),
               Container(height: 30, margin: EdgeInsets.symmetric(horizontal: 5),color: Colors.black54,width: 3,),
               InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      if(selectedValue != 9){
+                        hoverValue[9] =  value? true: false;
+                      }
+                    });
+                  },
                   onTap: () {
                     resetAllValues();
-                    hoverValue[9] = true;
-                  },child: Text("News",style: TextStyle(color: hoverValue[9] ? Colors.blue: Colors.black))),
+                    setState((){
+                      selectedValue = 9;
+                    });
+                  },child: Text("News",style: TextStyle(color: (hoverValue[9] || selectedValue == 9 ) ? Colors.blue: Colors.black))),
               Container(height: 30, margin: EdgeInsets.symmetric(horizontal: 5),color: Colors.black54,width: 3,),
               InkWell(
+                  onHover: (value) {
+                    setState(() {
+                      if(selectedValue != 10){
+                        hoverValue[10] =  value? true: false;
+                      }
+                    });
+                  },
                   onTap: () {
                     resetAllValues();
-                    hoverValue[10] = true;
-                  },child: Text("Clinical Trials",style: TextStyle(color: hoverValue[10] ? Colors.blue: Colors.black))),
+                    setState((){
+                      selectedValue = 10;
+                    });
+                  },child: Text("Clinical Trials",style: TextStyle(color: (hoverValue[10] || selectedValue == 10 ) ? Colors.blue: Colors.black))),
               SizedBox(width: 100,),
             ],
           ),
